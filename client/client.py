80f521bd03957a0df577fc64c5bce6760a36855f
sahartvk <sahartavakoli2002@gmail.com>
2022-03-31 23:45:24 +0430

    download function

--- client/client.py
+++ client/client.py
@@ -1,4 +1,6 @@
 import socket
+import sys
+
 
 server_ip = '127.0.0.1'
 server_port = 1234
@@ -8,6 +10,79 @@ print("connecting...")
 clientSocket.connect((server_ip, server_port))
 print("connected to server successfully")
 
-help_size = int(clientSocket.recv(4).decode())
-help = clientSocket.recv(help_size).decode()
-print(help)
+
+def help():
+    clientSocket.send("help".encode())
+
+    help_size = int(clientSocket.recv(4).decode())
+    help = clientSocket.recv(help_size).decode()
+    print(help)
+
+    return
+
+
+def dwld(file_name: str):
+    clientSocket.send("dwld".encode())
+    clientSocket.recv(1024)
+
+    # 1
+    clientSocket.send(str(sys.getsizeof(file_name)).encode())
+    clientSocket.send(file_name.encode())
+
+    # 2
+    file_size = int(clientSocket.recv(4).decode())
+    if file_size == -1:
+        print("there is no file with that name!")
+        return
+
+    clientSocket.send("ok".encode())
+
+    # 3
+    new_port = int(clientSocket.recv(1024).decode())
+
+    dwld_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    dwld_sock.connect((server_ip, new_port))
+    print("connected to new port successfully")
+
+    # 4
+    print("downloading...")
+    output_file = open(file_name, "wb")
+    file = dwld_sock.recv(file_size)
+    output_file.write(file)
+    print("downloaded successfully")
+    dwld_sock.send("ok".encode())
+    output_file.close()
+
+    # 5
+    dwld_sock.send("completed".encode())
+    dwld_sock.recv(1024)
+    dwld_sock.send("ok".encode())
+    dwld_sock.close()
+    print("download socket closed")
+
+    return
+
+
+# print help
+help()
+
+while True:
+
+    command = input(">>")
+    command.lower()
+
+    if command == "help":
+        help()
+    elif command == "list":
+        pass
+    elif command == "pwd":
+        pass
+    elif command[0:2] == "cd":
+        pass
+    elif command[0:4] == "dwld":
+        if command[5:]:
+            dwld(command[5:])
+    elif command == "quit":
+        pass
+    else:
+        print("invalid command!")
--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+import os
+import random
 
 
 server_ip = '127.0.0.1'
@@ -13,13 +15,86 @@ serverSocket.listen(5)
 sock, addr = serverSocket.accept()
 print(f"client connected by address {addr}")
 
-help = """
-HELP:   show help
-LIST:   list files
-PWD:    show current dir
-CD directory_name:  change directory
-DWLD file_path:    download file
-QUIT:   exit
-"""
-sock.send(str(sys.getsizeof(help)).encode())
-sock.send(help.encode())
+
+def help():
+    help = """
+    HELP:   show help
+    LIST:   list files
+    PWD:    show current dir
+    CD directory_name:  change directory
+    DWLD file_path:    download file
+    QUIT:   exit
+    """
+    sock.send(str(sys.getsizeof(help)).encode())
+    sock.send(help.encode())
+
+    return
+
+
+def dwld():
+    sock.send("ok".encode())
+
+    # 1
+    file_name_length = int(sock.recv(1024).decode())
+    file_name = sock.recv(file_name_length).decode()
+    print(f"file name: {file_name}")
+
+    # 2
+    if(os.path.isfile(file_name)):
+        print("exists")
+        sock.send(str(os.path.getsize(file_name)).encode())
+    else:
+        sock.send("-1".encode())
+        return
+
+    sock.recv(1024)
+
+    # 3
+    # generate a random number bitween 3000 to 50000 for port number
+    randport = random.randint(3000, 50000)
+    sock.send(str(randport).encode())
+
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # a new tcp connection for downloading
+    serverSocket.bind((server_ip, randport))
+    serverSocket.listen(1)
+    dwld_sock, addr = serverSocket.accept()
+    print(f"new socket for downloading on port '{randport}'")
+
+    # 4
+    # sending
+    print("sendin file...")
+    rfile = open(file_name, "rb")
+    file = rfile.read()
+    dwld_sock.send(file)
+    dwld_sock.recv(1024)
+    rfile.close()
+
+    # 5
+    print("completed")
+    dwld_sock.recv(1024)
+    dwld_sock.send("close".encode())
+    dwld_sock.recv(1024)
+    dwld_sock.close()
+    print("socket closed")
+    return
+
+
+while True:
+    command = sock.recv(1024).decode()
+    print(command)
+
+    if command == "help":
+        help()
+    elif command == "list":
+        pass
+    elif command == "pwd":
+        pass
+    elif command[0:2] == "cd":
+        pass
+    elif command[0:4] == "dwld":
+        dwld()
+    elif command == "quit":
+        pass
+    else:
+        pass
